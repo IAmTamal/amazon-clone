44fd739e8a2fea00258e6423280c6672e93cb99a
Tamal Das <gyansujan69@gmail.com>
2021-09-05 01:41:38 +0530

    User authetication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Checkout from "./Components/Checkout";
 import Login from "./Components/Login";
+import { useStateValue } from "./Components/StateProvider";
 
 function App() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     //These are basically routes to different types of pages
     <Router>
